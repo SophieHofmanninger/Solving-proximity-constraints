42565f34cf22dfc615e4bfae14e0bceb65216427
TurboNoobToober <jan.holzinger@gmx.at>
2019-05-19 15:57:17 +0200

    Created Generic Tuple class

---
+++ src/Tuple.java
@@ -0,0 +1,45 @@
+
+/**
+ * Generic Tuple Class
+ * @author Jan-Michael Holzinger & Sophie Hofmanninger
+ * @version 1.0
+ */
+public class Tuple<E> {
+
+	private E f,s;
+	
+	public Tuple(E first, E second) {
+		this.f=first;
+		this.s=second;
+	}
+
+	/**
+	 * @return the first element
+	 */
+	public E getFirst() {
+		return f;
+	}
+
+	/**
+	 * @param f the first element to set
+	 */
+	public void setFirst(E f) {
+		this.f = f;
+	}
+
+	/**
+	 * @return the second element
+	 */
+	public E getSecond() {
+		return s;
+	}
+
+	/**
+	 * @param s the second element to set
+	 */
+	public void setSecond(E s) {
+		this.s = s;
+	}
+	
+	
+}
